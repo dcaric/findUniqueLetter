ae0d3dacb611c8a751d4e8144fff8d0bcbade697
Dario Caric <dario.caric@gmail.com>
2024-01-13 20:41:02 +0100

    first commit

--- FindUniqueLetter/main.cpp
+++ FindUniqueLetter/main.cpp
@@ -6,9 +6,38 @@
 //
 
 #include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    string s = "adsgshjgfsdhj";
+    
+    map<char, int> myDict;
+
+    for (int i = 0; i < s.length(); i++) {
+        if ( i == 0) myDict.insert({s[i], 1});
+        else {
+            bool found = false;
+            for (const auto &p : myDict) {
+                if (p.first == s[i]) {
+                    myDict[p.first] = p.second + 1;
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                myDict.insert({s[i], 1});
+            }
+        }
+    }
+    
+    for (const auto &p : myDict) {
+        cout << p.first << " " << p.second << endl;
+    }
+    
+
     return 0;
 }
